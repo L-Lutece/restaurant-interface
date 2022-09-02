40325d7f0054a84269503028b0415c4331b42d24
Lutece <datsik771@outlook.com>
2022-09-02 14:37:18 -0300

    first commit

---
+++ Desktop/lub/lub/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div clasName="container">
+      <div className="App">
+          <h3>lol this is a lot damage</h3>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ Desktop/lub/lub/src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Header.css'
+
+function Header() {
+    return (
+    <div className="container">
+        <div className="Header">
+            <div className="items">
+                <a href="#">STACK</a>
+                <a href="#">EDUCAÇÃO</a>
+                <a href="#">PROJETOS</a>
+                <a href="#">EXPERIÊNCIA</a>
+                <a href="#">CONTATO</a>
+            </div>
+        </div>
+    </div>
+    );
+}
+
+export default Header;
